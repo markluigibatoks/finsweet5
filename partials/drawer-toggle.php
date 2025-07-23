d97d8474d3bd791801faf66d46fd965005b32d04
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-23 21:29:03 -0230

    fix: wcag issues

--- partials/drawer-toggle.php
+++ partials/drawer-toggle.php
@@ -4,4 +4,6 @@
     <span class="absolute left-0 top-2 w-6 h-1 bg-white rounded transition-all duration-300 ease-in-out opacity-100 group-[.open]:opacity-0"></span>
 
     <span class="absolute left-0 top-4 w-6 h-1 bg-white rounded transition-all duration-300 ease-in-out origin-center group-[.open]:top-1/2 group-[.open]:left-1/2 group-[.open]:-translate-x-1/2 group-[.open]:-translate-y-1/2 group-[.open]:rotate-[-45deg]"></span>
+
+    <span class="sr-only">Open navigation menu</span>
 </button>
