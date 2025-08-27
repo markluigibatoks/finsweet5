2a95aab7a6017bc286e83980aef3b3f27292212c
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-27 01:20:14 -0230

    add: our story

---
+++ page-our-story.php
@@ -0,0 +1,10 @@
+<?php 
+/*  Template Name: Our Story Page
+    Template Post Type: page
+*/
+  get_header();
+
+  get_template_part("/partials/our-story/section1");
+  
+  get_footer();
+?>
---
+++ partials/our-story/section1.php
@@ -0,0 +1,27 @@
+<div class="main-box lg:pt-24 pt-20">
+    <div class="wrapper">
+      <div class="lg:flex gap-5 justify-between">
+        <div class="lg:max-w-[544px] w-full text-center lg:text-left">
+            <h2 class="lg:mb-6 mb-5 xl:text-h1 lg:text-h2 md:text-h3 text-h4 text-white font-playfair">Our Story</h2>
+            <p>Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.</p>
+
+            <div class="flex gap-5 justify-center lg:justify-between">
+              <div class="mt-10 max-w-max">
+                <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/our-story/section1-img1.jpg" alt="autumn trees outside the cafe"/>
+              </div>
+
+              <div class="max-w-max">
+                <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/our-story/section1-img2.jpg" alt="cafe lights"/>
+              </div>
+            </div>
+        </div>
+
+        <div class="lg:-mt-4 mt-10 lg:max-w-[539px] w-full text-center lg:text-left">
+            <span></span>
+            <h2 class="lg:mb-6 mb-5 lg:text-h3 md:text-h4 text-h5 font-playfair text-white">One of the best cafes in <span class="text-colour-200">New York.</span></h2>
+            <p class="mb-4">Writing UX copies can be a little frustrating and confusing, and sometimes we are unsure about how to get the right word. To crack the code for the UX copies, we at Zeta Design wanted to build a Figma plugin for the larger design community. The plugin is called the Ghost UXWriter and has a set of UX copies cataloged and categorized with a voice and tone variation ranging from plain, casual to playful. The intention to build this Figma plugin originated from our Medium blog post, Designing voice and tone for error messages.</p>
+            <p>Writing UX copies can be a little frustrating and confusing, and sometimes we are unsure about how to get the right word. To crack the code for the UX copies, we at Zeta Design wanted to build a Figma plugin for the larger design community. The plugin is called the Ghost UXWriter and has a set of UX copies cataloged and categorized with a voice and tone variation ranging from plain, casual to playful. The intention to build this Figma plugin originated from our Medium blog post, Designing voice.</p>
+        </div>
+      </div>
+    </div>
+</div>
