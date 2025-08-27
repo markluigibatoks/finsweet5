17b1eae13624d587a4d21a3ad84dda1a20642d93
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-27 00:54:23 -0230

    add: section4

--- page-about.php
+++ page-about.php
@@ -7,6 +7,7 @@
   get_template_part("/partials/about-us/section1");
   get_template_part("/partials/about-us/section2");
   get_template_part("/partials/about-us/section3");
+  get_template_part("/partials/about-us/section4");
   
   get_footer();
 ?>
---
+++ partials/about-us/section4.php
@@ -0,0 +1,11 @@
+<div class="main-box lg:pt-38 pt-20">
+    <div class="wrapper">
+        <div class="w-full text-center lg:text-left lg:flex justify-between gap-5">
+            <h2 class="lg:mb-0 mb-5 opacity-87 lg:text-h2 md:text-h3 text-h4 text-white text-playfair lg:max-w-[564px]">Things our company stands for</h2>
+            <div class="lg:max-w-[538px]">
+              <p class="mb-6 opacity-87 lg:text-h3 text-h4 text-white font-playfair">Edit this text to make it your own. To edit, simply click directly</p>
+              <p>Writing UX copies can be a little frustrating and confusing, and sometimes we are unsure about how to get the right word. To crack the code for the UX copies, we at Zeta Design wanted to build a Figma plugin for the larger design community. The plugin is called the Ghost UXWriter and has a set of UX copies cataloged and categorized with a voice and tone variation ranging from plain, casual to playful. The intention to build this Figma plugin originated from our Medium blog post, 'Designing voice and tone for error messages.</p>
+            </div>
+        </div>
+    </div>
+</div>
