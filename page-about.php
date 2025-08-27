56b68977b42b994ca58e92c86f7e5657661bfdcf
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-27 00:21:43 -0230

    add: section1, 2

--- page-about.php
+++ page-about.php
@@ -4,19 +4,8 @@
 */
   get_header();
 
-  get_template_part(
-    '/partials/page-banner',
-    null,
-    array(
-      'image' => array(
-        'src' => get_template_directory_uri() . "/assets/images/about-us/placeholder.svg",
-        'alt' => "a place full of trees"
-      ),
-      'heading' => "Serving the world around us"
-    )
-  );
-
   get_template_part("/partials/about-us/section1");
+  get_template_part("/partials/about-us/section2");
   
   get_footer();
 ?>
---
+++ partials/about-us/section2.php
@@ -0,0 +1,13 @@
+<div class="main-box lg:py-31 py-10">
+    <div class="-z-10 absolute top-0 left-0 w-full h-full">
+      <img class="-z-10 object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section2-img1.jpg" alt="a busy cafe in the city"/>
+      
+      <div class="absolute top-0 left-0 bg-linear-to-r from-colour-0/92 from-21% to-52% to-colour-0/0 w-full h-full"></div>
+    </div>
+    <div class="z-10 wrapper relative">
+        <div class="max-w-[445px] w-full text-center lg:text-left">
+            <h2 class="lg:mb-6 mb-5 lg:text-h2 md:text-h3 text-h4 text-white">One of the best Cafes in <span class="text-colour-200">New York</span></h2>
+            <p>Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.. Through True Rich Attended does no end it his mother</p>
+        </div>
+    </div>
+</div>
