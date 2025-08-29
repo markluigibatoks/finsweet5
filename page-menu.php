cd4ffdecaecd76fda8f1068dfb1e9d12827ae081
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-29 18:54:40 -0230

    add: menu/section1

---
+++ page-menu.php
@@ -0,0 +1,11 @@
+<?php 
+/*  Template Name: Menu Page
+    Template Post Type: page
+*/
+  get_header();
+
+  get_template_part("/partials/menu/section1");
+  get_template_part("/partials/menu/section2");
+  
+  get_footer();
+?>
---
+++ partials/menu/section1.php
@@ -0,0 +1,17 @@
+<main class="main-box lg:pb-40 lg:pt-24 py-20">
+    <div class="wrapper">
+        <div class="flex flex-col lg:flex-row items-center justify-between gap-10 lg:gap-5">
+            <div class="lg:max-w-[613px] w-full text-center lg:text-left">
+                <div class="flex gap-4 items-center justify-center lg:justify-start">
+                    <span class="text-cap-1 font-playfair uppercase text-colour-200">Our Menu</span>
+                    <div class="flex items-center justify-center">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/images/line.svg'); ?>
+                    </div>
+                </div>
+                <h2 class="lg:mt-6 lg:mb-8 mb-5 lg:text-h2 text-h3 font-playfair text-white">Discover <span class="block">Our menu Chart</span></h2>
+            </div>
+
+            <p>Through True Rich Attended does no end it his mother since favourable real had half every him case in packages enquire we up ecstatic.. Through True Rich Attended does no end it his mother</p>
+        </div>
+    </div>
+</main>
