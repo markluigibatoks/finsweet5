30581767a0b635800442e80a93640004c668bf59
Mark-Luigi <markluigibatoks@gmail.com>
2025-08-29 19:18:07 -0230

    add: menu/section2

---
+++ partials/menu/section2.php
@@ -0,0 +1,113 @@
+<div class="main-box">
+    <div class="wrapper">
+        <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <li class="w-full">
+            <div class="w-full max-w-[405px] mx-auto">
+              <div class="max-w-max mx-auto">
+                <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/menu/section2-img1.jpg" alt="drp cofee"/>
+              </div>
+              <div class="py-6 px-8 bg-colour-400 flex gap-5 justify-between items-center">
+                <span class="lg:text-h4 text-h5 text-white font-playfair">Drp Cofee</span>
+                <span>$08.85</span>
+              </div>
+            </div>
+          </li>
+
+          <li class="w-full">
+            <div class="w-full max-w-[405px] mx-auto">
+              <div class="max-w-max mx-auto">
+                <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/menu/section2-img1.jpg" alt="drp cofee"/>
+              </div>
+              <div class="py-6 px-8 bg-colour-400 flex gap-5 justify-between items-center">
+                <span class="lg:text-h4 text-h5 text-white font-playfair">Drp Cofee</span>
+                <span>$08.85</span>
+              </div>
+            </div>
+          </li>
+
+          <li class="w-full">
+            <div class="w-full max-w-[405px] mx-auto">
+              <div class="max-w-max mx-auto">
+                <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/menu/section2-img1.jpg" alt="drp cofee"/>
+              </div>
+              <div class="py-6 px-8 bg-colour-400 flex gap-5 justify-between items-center">
+                <span class="lg:text-h4 text-h5 text-white font-playfair">Drp Cofee</span>
+                <span>$08.85</span>
+              </div>
+            </div>
+          </li>
+
+          <li class="w-full">
+            <div class="w-full max-w-[405px] mx-auto">
+              <div class="max-w-max mx-auto">
+                <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/menu/section2-img1.jpg" alt="drp cofee"/>
+              </div>
+              <div class="py-6 px-8 bg-colour-400 flex gap-5 justify-between items-center">
+                <span class="lg:text-h4 text-h5 text-white font-playfair">Drp Cofee</span>
+                <span>$08.85</span>
+              </div>
+            </div>
+          </li>
+
+          <li class="w-full">
+            <div class="w-full max-w-[405px] mx-auto">
+              <div class="max-w-max mx-auto">
+                <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/menu/section2-img1.jpg" alt="drp cofee"/>
+              </div>
+              <div class="py-6 px-8 bg-colour-400 flex gap-5 justify-between items-center">
+                <span class="lg:text-h4 text-h5 text-white font-playfair">Drp Cofee</span>
+                <span>$08.85</span>
+              </div>
+            </div>
+          </li>
+
+          <li class="w-full">
+            <div class="w-full max-w-[405px] mx-auto">
+              <div class="max-w-max mx-auto">
+                <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/menu/section2-img1.jpg" alt="drp cofee"/>
+              </div>
+              <div class="py-6 px-8 bg-colour-400 flex gap-5 justify-between items-center">
+                <span class="lg:text-h4 text-h5 text-white font-playfair">Drp Cofee</span>
+                <span>$08.85</span>
+              </div>
+            </div>
+          </li>
+
+          <li class="w-full">
+            <div class="w-full max-w-[405px] mx-auto">
+              <div class="max-w-max mx-auto">
+                <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/menu/section2-img1.jpg" alt="drp cofee"/>
+              </div>
+              <div class="py-6 px-8 bg-colour-400 flex gap-5 justify-between items-center">
+                <span class="lg:text-h4 text-h5 text-white font-playfair">Drp Cofee</span>
+                <span>$08.85</span>
+              </div>
+            </div>
+          </li>
+
+          <li class="w-full">
+            <div class="w-full max-w-[405px] mx-auto">
+              <div class="max-w-max mx-auto">
+                <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/menu/section2-img1.jpg" alt="drp cofee"/>
+              </div>
+              <div class="py-6 px-8 bg-colour-400 flex gap-5 justify-between items-center">
+                <span class="lg:text-h4 text-h5 text-white font-playfair">Drp Cofee</span>
+                <span>$08.85</span>
+              </div>
+            </div>
+          </li>
+
+          <li class="w-full">
+            <div class="w-full max-w-[405px] mx-auto">
+              <div class="max-w-max mx-auto">
+                <img class="object-cover w-full" src="<?php echo get_template_directory_uri();?>/assets/images/menu/section2-img1.jpg" alt="drp cofee"/>
+              </div>
+              <div class="py-6 px-8 bg-colour-400 flex gap-5 justify-between items-center">
+                <span class="lg:text-h4 text-h5 text-white font-playfair">Drp Cofee</span>
+                <span>$08.85</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+    </div>
+</div>
